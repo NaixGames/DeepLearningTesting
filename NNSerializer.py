7d7991d709e7372865b686a5a4d7f7a5283d8a57
Naix <91845220+NaixGames@users.noreply.github.com>
2026-03-29 16:22:53 +0100

    Serialization done!

--- NNSerializer.py
+++ NNSerializer.py
@@ -1,15 +1,59 @@
 from FFNN import FFNN
-
+import torch
+import Functions
 
 class NNSerializer:
-    def __init__(self, neural_network: FFNN):
-        self.neural_network = neural_network
+	def __init__(self, neural_network: FFNN):
+		self.neural_network = neural_network
 	
-    def save_weights(self, path: str) -> None:
-        #Need to iterate through params and save them.
-        #Note I will not save the activations functiosn themselves, but then it needs some consitency
-        pass
+	def save_weights(self, path: str) -> None:
+		torch.save(self.neural_network.layers_weights, path + "weights.pt")
+		torch.save(self.neural_network.layers_bias, path + "bias.pt")
 
-    def load_weights(self, path: str) -> None:
-        #Need to iterate through params and try to fetch the weights from some serialize file
-        pass
+	def load_weights(self, path: str) -> None:
+		#TODO: Some checks to make sure dimensions align?
+          
+		torch.serialization.add_safe_globals([torch.nn.modules.container.ParameterList])
+          
+		self.neural_network.layers_weights = torch.load(path + "weights.pt")
+		self.neural_network.layers_bias = torch.load(path + "bias.pt")
+
+
+if __name__ == "__main__":
+  sample_size = 100
+  possible_classes = 5
+  features_size = 5
+  inner_layers = [100, 200]
+  function_array = [Functions.sigmoid, Functions.tanh]
+
+  #Optional parameter for some functions
+  beta_swish = 0.5
+  alpha_celu = 0.75
+
+  assert(len(inner_layers) == len(function_array))
+
+  #We process the functions
+  derivatives_array = []
+  for i in range(0,len(function_array)):
+    derivatives_array.append(Functions.differentiate(function_array[i]))
+
+  for i in range(0, len(function_array)):
+    func = function_array[i]
+    #if we need an extra parameter, we "project" the function and the derivative
+    #Note we do this after we got the derivatives, mainly because, if not, checking the lambda would give an incorrect check,
+    #since the lambda results for "different definitions" is not the same
+    if (func == Functions.swish):
+      function_array[i] = lambda x : Functions.swish(x, beta_swish)
+      derivatives_array[i] = lambda x : Functions.swish_grad(x, beta_swish)
+    if (func == Functions.celu):
+      function_array[i] = lambda x : Functions.celu(x, alpha_celu)
+      derivatives_array[i] = lambda x : Functions.celu_grad(x, alpha_celu)
+
+  net = FFNN(features_size, inner_layers, function_array, derivatives_array, possible_classes)
+  print(net.summarize())
+
+
+  serializer = NNSerializer(net)
+  serializer.save_weights("SerializerTest/")
+  serializer.load_weights("SerializerTest/")
+  print(net.summarize())
