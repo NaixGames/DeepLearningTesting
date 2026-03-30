Adding different regularization types that I did during lunch. I think there is a mistake somewhere, as I am not getting good numerical results, but I will check that another.

FFNN.py
@@ -1,41 +1,45 @@
 import torch
 import Functions
 import CrossEntropy
+import math
 
 
 #TODO: Allow for this NN to work with general tensors as input data, instead of waiting for two dimensional
 #This assumption is seen when defining the first W tensor (in which I assume it is two dimensional)
 #and also when doing matrix multiplication in the forward and backward methods
 
-#TODO: Allow saving and loading training states
-
-
 class FFNN(torch.nn.Module):
-  def __init__(self, features_size : int, inner_layers_sizes : list[int], activation_functions : list[callable], derivative_functions : list[callable], categories_size : int):
+  def __init__(self, features_size : int, inner_layers_sizes : list[int], activation_functions : list[callable], derivative_functions : list[callable], categories_size : int, keep_prop = None, init_type : list[int] = None):
     super(FFNN, self).__init__()
     
     assert(len(activation_functions) == len(derivative_functions))
     assert(len(inner_layers_sizes) == len(activation_functions))
+    assert(keep_prop == None or len(keep_prop) == len(inner_layers_sizes) + 1)
 
     self.inner_layers = len(inner_layers_sizes)
     
     layers_w = []
     layers_b = []
     
-    layers_w.append(torch.randn(features_size, inner_layers_sizes[0]))
-    layers_b.append(torch.randn(1, inner_layers_sizes[0]))
-    
+    layers_w.append(self.give_weight_layer_init(features_size, inner_layers_sizes[0], 0, init_type))
+    layers_b.append(self.give_bias_layer_init(inner_layers_sizes[0]))
+
     for i in range(0, len(inner_layers_sizes)-1):
-      layers_w.append(torch.randn(inner_layers_sizes[i], inner_layers_sizes[i+1]))
-      layers_b.append(torch.randn(1, inner_layers_sizes[i+1]))
+      layers_w.append(self.give_weight_layer_init(inner_layers_sizes[i], inner_layers_sizes[i+1], i+1, init_type))
+      layers_b.append(self.give_bias_layer_init(inner_layers_sizes[i+1]))
       
-    layers_w.append(torch.randn(inner_layers_sizes[-1], categories_size))
-    layers_b.append(torch.randn(1, categories_size))
+    layers_w.append(self.give_weight_layer_init(inner_layers_sizes[-1], categories_size, len(inner_layers_sizes) + 1, init_type))
+    layers_b.append(self.give_bias_layer_init(categories_size))
 
     self.layers_weights = torch.nn.ParameterList([torch.nn.Parameter(layers_w[i]) for i in range(len(layers_w))])
     self.layers_bias = torch.nn.ParameterList([torch.nn.Parameter(layers_b[i]) for i in range(len(layers_b))])
     self.activation_functions = activation_functions
     self.derivative_functions = derivative_functions
+    self.keep_prop = keep_prop
+
+    if keep_prop is not None:
+      for prob in keep_prop:
+          assert(0 < prob <= 1)
 
   def summarize(self) -> str:
     result = "Summary: " + "\n"
@@ -45,35 +49,79 @@ class FFNN(torch.nn.Module):
       
     return result
   
+  def give_weight_layer_init(self, dim1 : int, dim2 : int, layer_depth : int, init_type : list[int]) -> torch.Tensor:
+    #init 0 : N(0,1)
+    #init 1 : Xavier
+    #init 2: He
+    if (init_type == None or layer_depth >= len(init_type) or init_type[layer_depth] == 0):
+      return torch.randn(dim1, dim2)
+    
+    if (init_type[layer_depth] == 1):
+      return torch.randn(dim1, dim2)*(math.sqrt(1/dim1))
+    
+    if (init_type[layer_depth] == 2):
+      return torch.randn(dim1, dim2)*(math.sqrt(2/dim1))
+
+  def give_bias_layer_init(self, dim : int) -> torch.Tensor:
+    return torch.torch.randn(1, dim)*0.01
+
   def forward(self, x : torch.Tensor) -> torch.Tensor:
     result = x
     u_results = []
     h_results = []
-  
 
     L = self.inner_layers
+
+    dropout_mask = []
+
+    #Apply droupout on first layer
+    if self.keep_prop is not None:
+        prob = self.keep_prop[0]
+        mask = (torch.rand_like(result) <= prob).float()
+        result = (result*mask)/prob
+        dropout_mask.append(mask)
 
     for i in range(0, L):
       result = result @ self.layers_weights[i] + self.layers_bias[i]
       u_results.append(result)
       result = self.activation_functions[i](result)
+
+      #apply droupout
+      if self.keep_prop is not None:
+        prob = self.keep_prop[i + 1]
+        mask = (torch.rand_like(result) <= prob).float()
+        result = (result*mask)/prob
+        dropout_mask.append(mask)
+
       h_results.append(result)
     
-    #last layer is different! It doesnt have an activation function, but we softmax
+    #last layer is different! It doesnt have an activation function, but we softmax. We also do not do dropout here
     result = result @ self.layers_weights[L] + self.layers_bias[L]
     u_results.append(result)
 
-    #store the reults of each later
+    #store the reults of each layer
     self.u_results = u_results
     self.h_results = h_results
+    self.dropout_mask = dropout_mask
 
     return Functions.softmax(result, 1)
   
-  def clear_grad(self):
-    for param in self.parameters():
-      if (param.grad is not None):
-        param.grad.zero_()
 
+  def predict(self, x : torch.Tensor) -> torch.Tensor:
+    #same function as forward, but we don't cache the evaluations on the node to backpropagate or use the dropout mask
+
+    result = x  
+    L = self.inner_layers
+
+    for i in range(0, L):
+      result = result @ self.layers_weights[i] + self.layers_bias[i]
+      result = self.activation_functions[i](result)
+ 
+    #last layer is different! It doesnt have an activation function, but we softmax
+    result = result @ self.layers_weights[L] + self.layers_bias[L]
+
+    return Functions.softmax(result, 1)
+  
 
   def backward(self, x, y, y_pred):
   
@@ -92,15 +140,39 @@ class FFNN(torch.nn.Module):
     # Propagate backwards; note first layer is different too!
 
     for i in range(L-1, 0, -1):
+      dL_dlasth = self.give_drop_grad(dL_dlasth, i+1)
+
       dL_dlastu = dL_dlasth * self.derivative_functions[i](self.u_results[i])
       self.layers_weights[i].grad = self.h_results[i-1].t() @ dL_dlastu
       self.layers_bias[i].grad = dL_dlastu.sum(dim=0, keepdim=True)
       dL_dlasth = dL_dlastu @ self.layers_weights[i].t()
 
-    # Propagate to first layer in which the h is the input
+    # Propagate to first layer in which the h is the input, also apply dropout
+    dL_dlasth = self.give_drop_grad(dL_dlasth, 1)
+
     dL_dlastu = dL_dlasth * self.derivative_functions[0](self.u_results[0])
-    self.layers_weights[0].grad = x.t() @ dL_dlastu
+
+    #apply dropout on input layer
+    x_masked = x
+    x_masked = self.give_drop_grad(x_masked, 0)
+
+    self.layers_weights[0].grad = x_masked.t() @ dL_dlastu
     self.layers_bias[0].grad = dL_dlastu.sum(dim=0, keepdim=True)
+
+
+  def give_drop_grad(self, grad : torch.Tensor, index : int):
+    if self.keep_prop is not None:
+        prob = self.keep_prop[index]
+        mask = self.dropout_mask[index]
+        return grad * mask/prob
+    
+    return grad
+
+  def clear_grad(self):
+    for param in self.parameters():
+      if (param.grad is not None):
+        param.grad.zero_()
+
 
   def numeric_grad_check(self, x: torch.Tensor, y : torch.Tensor, step : float = 1e-3) -> float:
     with torch.no_grad():

NNSerializer.py
@@ -10,50 +10,56 @@ class NNSerializer:
 		torch.save(self.neural_network.layers_weights, path + "weights.pt")
 		torch.save(self.neural_network.layers_bias, path + "bias.pt")
 
-	def load_weights(self, path: str) -> None:
-		#TODO: Some checks to make sure dimensions align?
-          
+	def load_weights(self, path: str) -> None:          
 		torch.serialization.add_safe_globals([torch.nn.modules.container.ParameterList])
-          
-		self.neural_network.layers_weights = torch.load(path + "weights.pt")
-		self.neural_network.layers_bias = torch.load(path + "bias.pt")
+					
+		loaded_weights = torch.load(path + "weights.pt")
+		loaded_bias =  torch.load(path + "bias.pt")
+
+		#check dimensions align before loading
+		assert(len(loaded_weights) == len(self.neural_network.layers_weights))
+		assert(len(loaded_bias) == len(self.neural_network.layers_bias))
+
+
+		self.neural_network.layers_weights = loaded_weights
+		self.neural_network.layers_bias = loaded_bias
 
 
 if __name__ == "__main__":
-  sample_size = 100
-  possible_classes = 5
-  features_size = 5
-  inner_layers = [100, 200]
-  function_array = [Functions.sigmoid, Functions.tanh]
+	sample_size = 100
+	possible_classes = 5
+	features_size = 5
+	inner_layers = [100, 200]
+	function_array = [Functions.sigmoid, Functions.tanh]
 
-  #Optional parameter for some functions
-  beta_swish = 0.5
-  alpha_celu = 0.75
+	#Optional parameter for some functions
+	beta_swish = 0.5
+	alpha_celu = 0.75
 
-  assert(len(inner_layers) == len(function_array))
+	assert(len(inner_layers) == len(function_array))
 
-  #We process the functions
-  derivatives_array = []
-  for i in range(0,len(function_array)):
-    derivatives_array.append(Functions.differentiate(function_array[i]))
+	#We process the functions
+	derivatives_array = []
+	for i in range(0,len(function_array)):
+		derivatives_array.append(Functions.differentiate(function_array[i]))
 
-  for i in range(0, len(function_array)):
-    func = function_array[i]
-    #if we need an extra parameter, we "project" the function and the derivative
-    #Note we do this after we got the derivatives, mainly because, if not, checking the lambda would give an incorrect check,
-    #since the lambda results for "different definitions" is not the same
-    if (func == Functions.swish):
-      function_array[i] = lambda x : Functions.swish(x, beta_swish)
-      derivatives_array[i] = lambda x : Functions.swish_grad(x, beta_swish)
-    if (func == Functions.celu):
-      function_array[i] = lambda x : Functions.celu(x, alpha_celu)
-      derivatives_array[i] = lambda x : Functions.celu_grad(x, alpha_celu)
+	for i in range(0, len(function_array)):
+		func = function_array[i]
+		#if we need an extra parameter, we "project" the function and the derivative
+		#Note we do this after we got the derivatives, mainly because, if not, checking the lambda would give an incorrect check,
+		#since the lambda results for "different definitions" is not the same
+		if (func == Functions.swish):
+			function_array[i] = lambda x : Functions.swish(x, beta_swish)
+			derivatives_array[i] = lambda x : Functions.swish_grad(x, beta_swish)
+		if (func == Functions.celu):
+			function_array[i] = lambda x : Functions.celu(x, alpha_celu)
+			derivatives_array[i] = lambda x : Functions.celu_grad(x, alpha_celu)
 
-  net = FFNN(features_size, inner_layers, function_array, derivatives_array, possible_classes)
-  print(net.summarize())
+	net = FFNN(features_size, inner_layers, function_array, derivatives_array, possible_classes)
+	print(net.summarize())
 
 
-  serializer = NNSerializer(net)
-  serializer.save_weights("SerializerTest/")
-  serializer.load_weights("SerializerTest/")
-  print(net.summarize())
+	serializer = NNSerializer(net)
+	serializer.save_weights("SerializerTest/")
+	serializer.load_weights("SerializerTest/")
+	print(net.summarize())

SGD.py
@@ -7,13 +7,14 @@ from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as functional_parse
 
 class SGD():
-  def __init__(self, network : FFNN, rate : float = 1e-3):
+  def __init__(self, network : FFNN, rate : float = 1e-3, decay = 0):
     self.network = network
     self.rate = rate
+    self.decay = decay
   
   def step(self):
     for param in self.network.parameters():
-      param.data -= param.grad*self.rate
+      param.data = (1-self.decay)*param.data - param.grad*self.rate
 
   def train_FFNN(self, net : FFNN, dataset : Dataset, epochs : int =1, batch_size : int =1, print_frequency : int = 10):
     for i in range(epochs):
@@ -56,10 +57,14 @@ if __name__ == "__main__":
   possible_classes = 5
   features_size = 5
   inner_layers = [20, 20, 20, 20, 20, 20, 20, 20]
-  function_array = [Functions.relu, Functions.relu, Functions.relu, Functions.relu, Functions.relu, Functions.relu, Functions.relu, Functions.relu]
+  inner_layers = [20, 20, 20]
+  function_array = [Functions.relu, Functions.relu, Functions.relu]
+
+  keep_prob = [0.9, 0.75, 0.75, 0.75]
+  init_type = [2, 2, 2, 2, 2]
 
   #Param for optimizer
-  step = 0.00001
+  step = 0.01
   frequency_loss_print = 1000
   epochs = 10000
   batch_size = 100
@@ -91,13 +96,13 @@ if __name__ == "__main__":
       function_array[i] = lambda x : Functions.celu(x, alpha_celu)
       derivatives_array[i] = lambda x : Functions.celu_grad(x, alpha_celu)
 
-  net = FFNN.FFNN(features_size, inner_layers, function_array, derivatives_array, possible_classes)
+  net = FFNN.FFNN(features_size, inner_layers, function_array, derivatives_array, possible_classes, keep_prop, init_type)
   # --- Set device to GPU ---
   net.to(device)
 
   # --- train ---
 
-  dataset = RandomDataset.RandomDataset(sample_size, features_size, possible_classes, "cuda")
+  dataset = RandomDataset.RandomDataset(sample_size, features_size, possible_classes, device)
 
   print("Input:")
   print(dataset.x)
@@ -107,8 +112,20 @@ if __name__ == "__main__":
   print("real classes")
   print(dataset.y)
 
-  optimizer = SGD(net, step)
+  optimizer = SGD(net, step, 0.0001)
   optimizer.train_FFNN(net, dataset, epochs, batch_size, frequency_loss_print)
 
+  print("Dataset")
+  input = dataset.x
+  print(input)
+
+  print("Predicted for dataset")
+  print(net.predict(input))
+  print("Real answer")
+  print(dataset.y)
+
+
+  print("Net")
+  print(net.summarize())
 
 